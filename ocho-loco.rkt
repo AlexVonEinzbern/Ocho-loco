70ce11c60813553e507c1de7580b28cc5a962279
Alexander Díaz <m_alexander_d@outlook.es>
2018-05-11 18:21:45 -0500

    ocho-loco

--- ocho-loco.rkt
+++ ocho-loco.rkt
@@ -5,4 +5,4 @@
 
 (define ventana (open-viewport "Ocho-Loco" 800 700))
 
-((draw-pixmap ventana) "images/2_of_clubs.png" (make-posn 0.0 0.0)"black")
+((draw-pixmap ventana) "images/2_of_clubs.png" (make-posn 0.0 0.0))
